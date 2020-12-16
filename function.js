773cc0f4a177371a6732bd416e32022fac3b2503
Stefania Popa <stefania_florina10@yahoo.com>
2020-12-16 18:07:44 +0200

    add form and delete and add button

--- function.js
+++ function.js
@@ -1,8 +1,17 @@
 console.log('test script');
 
 function insertPersons(person){
-    
     var tbody = document.querySelector('#list tbody');
+    var firstName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+    var link = document.getElementById("link").value;
+    var row = tbody.insertRow(-1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    cell1.innerHTML = firstName;
+    cell2.innerHTML = lastName;
+    cell3.innerHTML = link;
     tbody.innerHTML = getPersonsHtml(person);
 }
 
@@ -15,14 +24,19 @@ function getPersonHtml(person){
     return `<tr>
         <td>${person.firstName}</td>
         <td>${person.lastName}</td>
-        <td><a target="_blank" href="https://github.com/${link}" class="fa fa-github" aria-hidden="true"></a></td>
+        <td><a target="_blank" href="https://github.com/${person.gitHub}" class="fa fa-github" aria-hidden="true"></a></td>
         <td><a target="_blank" href="https://www.linkedin.com/in/${link}/" class="fa fa-linkedin"></a></td>
     </tr>`;
 }
-
 
 fetch('team.json')
     .then(res => res.json())
     .then(data =>  {
             insertPersons(data);
     });
+
+      
+function deleteRow() {
+    document.getElementById("list").deleteRow(-1);
+}
+ 
