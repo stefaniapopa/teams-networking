9e4a6406426bb032dcdd5766066e401369f35cae
Stefania Popa <stefania_florina10@yahoo.com>
2021-01-11 21:47:07 +0200

    did addeventlisteners

--- function.js
+++ function.js
@@ -1,23 +1,21 @@
 const API = {
     CREATE: {
-        URL: "create.json",
-        METHOD: "GET" // POST
+        URL: "http://localhost:3000//teams-json/create",
+        METHOD: "POST"
     },
     READ: {
-        URL: "team.json",
+        URL: "http://localhost:3000/teams-json",
         METHOD: "GET"
     },
     UPDATE: {
-        URL: "",
-        METHOD: "GET"
+        URL: "http://localhost:3000//teams-json/update",
+        METHOD: "PUT"
     },
     DELETE: {
-        URL: "delete.json",
-        METHOD: "GET" //DELETE
+        URL: "http://localhost:3000/teams-json/delete",
+        METHOD: "DELETE"
     }
 };
-
-API.READ.URL
 
 function insertPersons(persons) {
     var tbody = document.querySelector('#list tbody');
@@ -29,14 +27,13 @@ function getPersonsHtml(persons) {
 }
 
 function getPersonHtml(person) {
-    var link = person.link;
+    const gitHub = person.gitHub
     return `<tr>
         <td>${person.firstName}</td>
         <td>${person.lastName}</td>
-        <td><a target="_blank" href="https://github.com/${person.gitHub}" class="fa fa-github" aria-hidden="true"> </a>
-        <a target="_blank" href="https://www.linkedin.com/in/${link}/" class="fa fa-linkedin"></a></td>
+        <td><a target="_blank" href="https://github.com/${gitHub}" class="fa fa-github" aria-hidden="true"> </a>
         <td>
-            <a href="${API.DELETE.URL}?id=${person.id}">&#10006;</a>
+            <a href="#" class="delete-row" data-id="${person.id}">&#10006;</a>
         </td>
     </tr>`;
 }
@@ -45,64 +42,89 @@ let allPersons = [];
 
 function loadList() {
     fetch(API.READ.URL)
-    .then(res => res.json())
-    .then(data => {
-        allPersons = data;
-        insertPersons(data);
-    });
+        .then(res => res.json())
+        .then(data => {
+            allPersons = data;
+            insertPersons(data);
+        });
 }
 
 loadList();
 
-function searchPersons(text){
+function searchPersons(text) {
     text = text.toLowerCase();
     console.warn('ai cautat', text, allPersons);
-    return allPersons.filter(person =>{
+    return allPersons.filter(person => {
         return person.firstName.toLowerCase().indexOf(text) > -1 ||
-        person.lastName.toLowerCase().indexOf(text) > -1 ;
+            person.lastName.toLowerCase().indexOf(text) > -1;
     });
 }
 
-const search = document.getElementById('search');
-search.addEventListener('input', e => {
-    const text = e.target.value
-    
-    const filtrate = searchPersons(text);
-
-    insertPersons(filtrate);
-});
-
-function saveTeamMembers(){
+function saveTeamMembers() {
     const firstName = document.querySelector('input[name=firstName]').value;
     const lastName = document.querySelector('input[name=lastName]').value;
     const gitHub = document.querySelector('input[name=gitHub]').value;
 
-    const person ={
+    const person = {
         firstName,
         lastName,
-        gitHub
+        gitHub: gitHub
     };
     console.log('saving', person);
 
     fetch(API.CREATE.URL, {
         method: API.CREATE.METHOD,
-        body: API.CREATE.METHOD === "GET" ?  null : JSON.stringify(person)
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: API.CREATE.METHOD === "GET" ? null : JSON.stringify(person)
     })
         .then(res => res.json())
         .then(r => {
             console.warn(r);
-            if( r.success){
-                alert('saving data..., please wait until we are ready.');
-                    console.log("refresh list");
-                    loadList();               
-            
-            } 
+            if (r.success) {
+                loadList();
+
+            }
         });
 }
 
-const saveBtn = document.querySelector('#list button');
-saveBtn.addEventListener('click', () => {
-    saveTeamMembers();
-});
+function deleteTeamMember(id) {
+    fetch("http://localhost:3000/teams-json/delete", {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ id })
+    });
+}
+
+function addEventListeners() {
+    const search = document.getElementById('search');
+    search.addEventListener('input', e => {
+        const text = e.target.value
+
+        const filtrate = searchPersons(text);
+        console.log({ filtrate })
+
+        insertPersons(filtrate);
+    });
+
+    const saveBtn = document.querySelector('#list tfoot button');
+    saveBtn.addEventListener('click', () => {
+        saveTeamMembers();
+    });
+
+    const table = document.querySelector('#list tbody');
+    table.addEventListener('click', (e) => {
+        const target = e.target;
+        if (e.target.matches("a.delete-row")) {
+            const id = target.getAttribute("data-id");
+            deleteTeamMember(id)
+        }
+    });
+}
+
+addEventListeners();
 
 
